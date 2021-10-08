35737ffe74d475adbfcec2498cb64e15599e17c2
Tozuka Yuji <yuji_tozuka@ydktechs.co.jp>
2021-10-08 14:56:22 +0900

    amazon

--- amazon.py
+++ amazon.py
@@ -5,6 +5,7 @@ load_dotenv() #環境変数のロード
 from logger import set_logger
 logger = set_logger(__name__)
 from sp_api.api import Catalog
+from sp_api.api import ProductFees
 from sp_api.base.marketplaces import Marketplaces
 import pandas as pd
 
@@ -15,13 +16,27 @@ def fetch_amazon_item_price(jan_list):
     amazon_item_list = []
     for jan in jan_list:
         jan = jan[0]
-        res = Catalog(Marketplaces.JP).list_items(JAN='4530107941624')
-        item_data = res.payload.get('Items')[0]
+        res = Catalog(Marketplaces.JP).list_items(JAN=jan)
+        item_list = res.payload.get('Items')
+        item_count = len(item_list)
+        for n in range(item_count):
+            if n == 0:    
+                price = item_list[n].get('AttributeSets')[0].get('ListPrice').get('Amount')
+                asin = item_list[n].get('Identifiers').get('MarketplaceASIN').get('ASIN')
+            elif price > item_list[n].get('AttributeSets')[0].get('ListPrice').get('Amount'):
+                price = item_list[n].get('AttributeSets')[0].get('ListPrice').get('Amount')
+                asin = item_list[n].get('Identifiers').get('MarketplaceASIN').get('ASIN')
+        item_url = f'https://www.amazon.co.jp/gp/product/{asin}'
+        fees_data = ProductFees(Marketplaces.JP).get_product_fees_estimate_for_asin(asin=asin,price=price,currency='JPY')
+        fees = fees_data.payload.get('FeesEstimateResult').get('FeesEstimate').get('TotalFeesEstimate').get('Amount')
+        price = price - fees
         
-        asin = item_data
 
 
-    print(res)
 
+
+
+            
+            
 if __name__ == "__main__":
     fetch_amazon_item_price()
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ load_dotenv() #環境変数のロード
 from logger import set_logger
 logger = set_logger(__name__)
 from sp_api.api import Catalog
+from sp_api.api import ProductFees
 from sp_api.base.marketplaces import Marketplaces
 import pandas as pd
 
@@ -16,27 +17,19 @@ import pandas as pd
 
 res = Catalog(Marketplaces.JP).list_items(JAN='4530107941624')
 
-item_list = res.payload.get('Items')[0].get('Identifiers').get('MarketplaceASIN').get('ASIN')
+item = res.payload.get('Items')[0].get('AttributeSets')[0].get('ListPrice').get('Amount')
+asin = res.payload.get('Items')[0].get('Identifiers').get('MarketplaceASIN').get('ASIN')
 
-item = res.payload.get('Items')
-
-
-print(len(item))
+category = res.payload.get('Items')[0].get('AttributeSets')[0].get('ProductGroup')
 
 
 
+fees_data = ProductFees(Marketplaces.JP).get_product_fees_estimate_for_asin(asin='B077QV8NXH',price=1600,currency='JPY')
 
-[{'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B077QV8NXH'}}, 'AttributeSets': [{'Binding': 'ヘルスケア&ケア用品', 'Brand': 'OCEAN TRICO(オーシャントリコ)', 'Color': 'ブルー', 'Flavor': 'シャインオーバー ツヤ×キープ', 'ItemDimensions': {'Weight': {'value': 0.01, 'Units': 'pounds'}}, 'IsAdultProduct': False, 'Label': 'フィッツコーポレーション', 'ListPrice': {'Amount': 1650.0, 'CurrencyCode': 'JPY'}, 'Manufacturer': 'フィッツコーポレーション', 'PackageDimensions': {'Height': {'value': 2.2440944859, 'Units': 'inches'}, 'Length': {'value': 2.9527559025, 'Units': 'inches'}, 'Width': {'value': 2.2834645646, 'Units': 'inches'}, 'Weight': {'value': 0.2866009406, 'Units': 'pounds'}}, 'PackageQuantity': 1, 'PartNumber': '311232018', 'ProductGroup': 'Beauty', 'ProductTypeName': 'HAIR_STYLING_AGENT', 'Publisher': 'フィッツコーポ 
-レーション', 'ReleaseDate': '2018-04-14', 'Size': '80グラム (x 1)', 'SmallImage': {'URL': 'https://m.media-amazon.com/images/I/41B428MEXuL._SL75_.jpg', 'Height': {'Units': 'pixels', 'value': 75}, 'Width': {'Units': 'pixels', 'value': 75}}, 'Studio': 'フィッツコーポレーション', 'Title': 'オーシャントリコ ヘアワックス(シャイ
-ンオーバー) ツヤ×キープ'}], 'Relationships': [{'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B0822J5ZRZ'}}}], 'SalesRankings': [{'ProductCategoryId': 'beauty_display_on_website', 'Rank': 1347}, {'ProductCategoryId': '169739011', 'Rank': 27}]}, {'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B07ZTDB7PF'}}, 'AttributeSets': [{'Binding': 'セット買い', 'Brand': 'OCEAN TRICO(オーシャントリコ)', 'ProductGroup': 'Beauty', 'ProductTypeName': 'HAIR_STYLING_AGENT', 'Size': '2個 
-アソート', 'SmallImage': {'URL': 'https://m.media-amazon.com/images/I/311KEgvM9AL._SL75_.jpg', 'Height': {'Units': 'pixels', 'value': 75}, 'Width': {'Units': 'pixels', 'value': 75}}, 'Title': '【セット買い】オーシャ 
-ントリコ ヘアワックス(シャインオーバー) ツヤ×キープ & ヘアワックス(エアー) エアリー×キープ'}], 'Relationships': [{'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B0822J5ZRZ'}}}], 'SalesRankings': [{'ProductCategoryId': 'beauty_display_on_website', 'Rank': 1347}, {'ProductCategoryId': '169739011', 'Rank': 27}]}, {'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B07ZTDSVVS'}}, 'AttributeSets': [{'Binding': 'セット買い', 'Brand': 'OCEAN TRICO(オーシャントリコ)', 'ProductGroup': 'Beauty', 'ProductTypeName': 'HAIR_STYLING_AGENT', 'Size': '2個アソート', 'SmallImage': {'URL': 'https://m.media-amazon.com/images/I/31Kxt0OuHSL._SL75_.jpg', 'Height': {'Units': 'pixels', 'value': 75}, 'Width': {'Units': 'pixels', 'value': 75}}, 'Title': '【セット買い】オーシャントリコ ヘアワックス(シャインオーバー) ツ 
-ヤ×キープ & ヘアワックス(クレイ) ボリューム×キープ'}], 'Relationships': [{'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B0822J5ZRZ'}}}], 'SalesRankings': [{'ProductCategoryId': 'beauty_display_on_website', 'Rank': 1347}, {'ProductCategoryId': '169739011', 'Rank': 27}]}, {'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B07ZTDYMLT'}}, 'AttributeSets': [{'Binding': 'セット買い', 'Brand': 'OCEAN TRICO(オーシャントリコ)', 'ProductGroup': 'Beauty', 'ProductTypeName': 'HAIR_STYLING_AGENT', 'Size': '2個アソート', 'SmallImage': {'URL': 'https://m.media-amazon.com/images/I/31ej9DU-QAL._SL75_.jpg', 'Height': {'Units': 'pixels', 'value': 75}, 'Width': {'Units': 'pixels', 'value': 75}}, 'Title': '【セット買い】オーシャントリコ ヘアワックス(シャインオーバー) ツヤ×キープ & ヘアワックス(エッジ) シャープ
-×キープ'}], 'Relationships': [{'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B0822J5ZRZ'}}}], 'SalesRankings': [{'ProductCategoryId': 'beauty_display_on_website', 'Rank': 1347}, {'ProductCategoryId': '169739011', 'Rank': 27}]}, {'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B07ZTFLRNV'}}, 'AttributeSets': [{'Binding': 'セット買い', 'Brand': 'OCEAN TRICO(オーシャ
-ントリコ)', 'ProductGroup': 'Beauty', 'ProductTypeName': 'HAIR_STYLING_AGENT', 'Size': '2個アソート', 'SmallImage': {'URL': 'https://m.media-amazon.com/images/I/31aSDvxE+XL._SL75_.jpg', 'Height': {'Units': 'pixels', 
-'value': 75}, 'Width': {'Units': 'pixels', 'value': 75}}, 'Title': '【セット買い】オーシャントリコ ヘアワッ 
-クス(シャインオーバー) ツヤ×キープ & OCEAN TRICO(オーシャントリコ) オーシャントリコ ヘアワックス(オーバード 
-ライブ) マット×キープ オーバードライブ 単品 80g'}], 'Relationships': [{'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B0822J5ZRZ'}}}], 'SalesRankings': [{'ProductCategoryId': 'beauty_display_on_website', 'Rank': 1347}, {'ProductCategoryId': '169739011', 'Rank': 27}]}, {'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B07ZTDV3DS'}}, 'AttributeSets': [{'Binding': 'セ 
-ット買い', 'Brand': 'OCEAN TRICO(オーシャントリコ)', 'ProductGroup': 'Beauty', 'ProductTypeName': 'HAIR_STYLING_AGENT', 'Size': '2個アソート', 'SmallImage': {'URL': 'https://m.media-amazon.com/images/I/31WIGbHyQVL._SL75_.jpg', 'Height': {'Units': 'pixels', 'value': 75}, 'Width': {'Units': 'pixels', 'value': 75}}, 'Title': 
-'【セット買い】オーシャントリコ ヘアワックス(シャインオーバー) ツヤ×キープ & ヘアワックス(ナチュラル) ルーズ
-×キープ'}], 'Relationships': [{'Identifiers': {'MarketplaceASIN': {'MarketplaceId': 'A1VC38T7YXB528', 'ASIN': 'B0822J5ZRZ'}}}], 'SalesRankings': [{'ProductCategoryId': 'beauty_display_on_website', 'Rank': 1347}, {'ProductCategoryId': '169739011', 'Rank': 27}]}]
+fees = fees_data.payload.get('FeesEstimateResult').get('FeesEstimate').get('TotalFeesEstimate').get('Amount')
+
+print(fees)
+
+
+
+
